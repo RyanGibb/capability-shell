619d26dbb1b37c00d7e1bdf3e5167fc9d66b4294
Ryan Gibb <ryan@freumh.org>
2024-12-17 16:35:16 +0000

    initial

---
+++ bin/shelld.ml
@@ -0,0 +1,56 @@
+let capnp_serve env state_dir vat_config mosh_addr ~sw =
+  let services =
+    let make_sturdy = Capnp_rpc_unix.Vat_config.sturdy_uri vat_config in
+    Capnp_rpc_net.Restorer.Table.create make_sturdy
+  in
+  let restore = Capnp_rpc_net.Restorer.of_table services in
+
+  let name = Unix.gethostname () in
+  let id = Capnp_rpc_unix.Vat_config.derived_id vat_config name in
+  let host = Cap.Host.local ~env ~sw ~name ~mosh_addr in
+  Capnp_rpc_net.Restorer.Table.add services id host;
+  let vat = Capnp_rpc_unix.serve ~sw ~restore vat_config in
+
+  let cap_dir = Eio.Path.(env#fs / state_dir) in
+  Eio.Path.mkdirs ~exists_ok:true ~perm:0o750 cap_dir;
+  let _, cap_file = Eio.Path.(cap_dir / (name ^ ".cap")) in
+  match Capnp_rpc_unix.Cap_file.save_service vat id cap_file with
+  | Error (`Msg m) -> failwith m
+  | Ok () -> ()
+
+let run env state_dir mosh_addr vat_config =
+  Mirage_crypto_rng_eio.run (module Mirage_crypto_rng.Fortuna) env @@ fun () ->
+  Eio.Switch.run @@ fun sw ->
+  capnp_serve env state_dir vat_config mosh_addr ~sw;
+  Eio.Fiber.await_cancel ()
+
+let () =
+  Eio_main.run @@ fun env ->
+  Logs.set_level (Some Logs.Info);
+  Logs.set_reporter (Logs_fmt.reporter ());
+  (* Logs.Src.set_level Capnp_rpc.Debug.src (Some Logs.Debug); *)
+  let open Cmdliner in
+  let cmd =
+    let state_dir =
+      let doc =
+        "Directory to state such as account keys, sturdy refs, and \
+         certificates."
+      in
+      Arg.(value & opt string "state" & info [ "state-dir" ] ~doc)
+    in
+    let mosh_addr =
+      let doc = "IP address of mosh server" in
+      let ip = Arg.conv (Ipaddr.of_string, Ipaddr.pp) in
+      Arg.(value & opt (some ip) None & info [ "mosh-ip" ] ~doc)
+    in
+    let term =
+      Term.(
+        const (run env)
+        $ state_dir $ mosh_addr
+        $ Capnp_rpc_unix.Vat_config.cmd env)
+    in
+    let doc = "shelld" in
+    let info = Cmd.info "shelld" ~doc in
+    Cmd.v info term
+  in
+  exit (Cmd.eval cmd)
---
+++ lib/cap/host.ml
@@ -0,0 +1,124 @@
+open Raw
+open Capnp_rpc
+
+let local ~env ~sw ~name ~mosh_addr =
+  let module Host = Api.Service.Host in
+  Host.local
+  @@ object
+       inherit Host.service
+
+       method get_info_impl _ release_param_caps =
+         let open Host.GetInfo in
+         release_param_caps ();
+         let response, results = Service.Response.create Results.init_pointer in
+         let info = Results.info_init results in
+         Api.Builder.HostInfo.name_set info name;
+         Service.return response
+
+       method shell_impl params release_param_caps =
+         let open Host.Shell in
+         let exitCallback = Option.get @@ Params.exit_callback_get params in
+         release_param_caps ();
+
+         let pty = Pty.open_pty () in
+         (* spawn shell as child process *)
+         let shell =
+           let pw = Unix.getpwuid (Unix.getuid ()) in
+           let ptyAction = Fork_actions.setup_shell pty
+           and execvAction =
+             Eio_linux.Low_level.Process.Fork_action.execve
+               pw.pw_shell
+               (* The shell name is preceded by '-' to indicate
+                  that this is a login shell. *)
+               ~argv:[| "-bash" |] (* ^ Filename.basename pw.pw_shell *)
+               ~env:(Unix.unsafe_environment ())
+           in
+           Eio_linux.Low_level.Process.spawn ~sw [ ptyAction; execvAction ]
+         in
+         (* don't close PTY file descriptors *)
+         let close_unix = false in
+         let sink =
+           Eio_unix.Net.import_socket_stream ~sw ~close_unix pty.Pty.masterfd
+         in
+         let source =
+           Eio_unix.Net.import_socket_stream ~sw ~close_unix pty.Pty.masterfd
+         in
+         let stdin data = Eio.Flow.write sink [ Cstruct.of_string data ] in
+
+         Eio.Fiber.fork ~sw (fun () ->
+             let e =
+               Eio.Promise.await (Eio_linux.Low_level.Process.exit_status shell)
+             in
+             match Exit_callback.exitStatus exitCallback e with
+             | Ok () -> ()
+             | Error (`Capnp e) ->
+                 Eio.traceln "Error calling Exit_callback.exitStatus %a"
+                   Capnp_rpc.Error.pp e);
+
+         let buf = Cstruct.create 4096 in
+         let stdout () =
+           let got = Eio.Flow.single_read source buf in
+           Cstruct.to_string (Cstruct.sub buf 0 got)
+         in
+         let process = Process.local ~stdin ~stdout ~stderr:stdout in
+
+         let response, results = Service.Response.create Results.init_pointer in
+         Results.process_set results (Some process);
+         Service.return response
+
+       method mosh_impl _ release_param_caps =
+         let open Host.Mosh in
+         release_param_caps ();
+         match mosh_addr with
+         | None -> Service.fail ""
+         | Some mosh_addr ->
+             let proc_mgr = Eio.Stdenv.process_mgr env in
+             let buf = Buffer.create 80 in
+             let () =
+               Eio.Process.run proc_mgr ~stdout:(Eio.Flow.buffer_sink buf)
+                 [ "mosh-server" ]
+             in
+             let string = Buffer.contents buf in
+             let split = String.split_on_char ' ' string in
+             let port = Int32.of_string (List.nth split 2) in
+             let key =
+               String.trim
+                 (String.concat ""
+                    (String.split_on_char '\n' (List.nth split 3)))
+             in
+             let response, results =
+               Service.Response.create Results.init_pointer
+             in
+             let mosh_connect = Results.mosh_connect_init results in
+             Api.Builder.MoshConnect.ip_set mosh_connect
+               (Ipaddr.to_string mosh_addr);
+             Api.Builder.MoshConnect.port_set mosh_connect port;
+             Api.Builder.MoshConnect.key_set mosh_connect key;
+             Service.return response
+     end
+
+let get_info t () =
+  let open Api.Client.Host.GetInfo in
+  let request = Capability.Request.create_no_args () in
+  let ( let* ) = Result.bind in
+  let* result = Capability.call_for_value t method_id request in
+  Ok (Results.info_get result)
+
+let shell t exitCallback =
+  let open Api.Client.Host.Shell in
+  let request, params = Capability.Request.create Params.init_pointer in
+  Params.exit_callback_set params (Some exitCallback);
+  Capability.call_for_caps t method_id request Results.process_get_pipelined
+
+type mosh_connect = { ip : Ipaddr.t; port : int32; key : string }
+
+let mosh t () =
+  let open Api.Client.Host.Mosh in
+  let request = Capability.Request.create_no_args () in
+  let ( let* ) = Result.bind in
+  let* result = Capability.call_for_value t method_id request in
+  let mosh_connect = Results.mosh_connect_get result in
+  let* ip = Ipaddr.of_string (Api.Reader.MoshConnect.ip_get mosh_connect) in
+  let port = Api.Reader.MoshConnect.port_get mosh_connect in
+  let key = Api.Reader.MoshConnect.key_get mosh_connect in
+  Ok { ip; port; key }
